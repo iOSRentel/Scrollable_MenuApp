48b87722c08acd8594ab5f57c138a44db39bc09e
Boris Zinovyev <BZ@rentel.me>
2022-02-05 21:16:33 +0300

    Initial Commit

---
+++ Scrollable_MenuApp/Scrollable_MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrollable_MenuAppApp.swift
+//  Scrollable_MenuApp
+//
+//  Created by Boris Zinovyev on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Scrollable_MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
